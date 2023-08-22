f041926f7167bdeecd3e0caebb703ce693e285f4
lohanlhn <lohan.massote@gmail.com>
2023-08-22 20:10:29 -0300

    feat: Implements the method to display contacts

--- Program.cs
+++ Program.cs
@@ -2,6 +2,48 @@
 {
     internal class Program
     {
+        static void Main(string[] args)
+        {
+            //armazena os dados da agenda
+            string[] nome = new string[200];
+            string[] email = new string[200];
+
+            //tamanho logico
+            int tl = 0;
+            int op = 0;
+
+            //dados temporarios
+            nome[tl] = "Joao Marcos";
+            email[tl] = "joao@email.com";
+            tl++;
+            nome[tl] = "Maria Joana";
+            email[tl] = "maria@email.com";
+            tl++;
+            nome[tl] = "Erica Rodrigues";
+            email[tl] = "erica@email.com";
+            tl++;
+
+            while (op != 6)
+            {
+                op = ExibirMenu();
+                switch (op)
+                {
+                    case 1:
+                        ExibirContatos(nome, email, tl);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                }
+            }            
+
+        }
+
         static int ExibirMenu()
         {
             int op = 0;
@@ -15,36 +57,18 @@
             Console.WriteLine("Sair - 6");
             Console.WriteLine("Opção: ");
             op = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
             return op;
         }
-        static void Main(string[] args)
+
+        static void ExibirContatos(string[] nome, string[] email, int tl)
         {
-            //armazena os dados da agenda
-            string[] nome = new string[200];
-            string[] email = new string[200];
-
-            //tamanho logico
-            int tl = 0;
-            int op = 0;
-
-            while (op != 6)
+            for (int i = 0; i < tl; i++)
             {
-                op = ExibirMenu();
-                switch (op)
-                {
-                    case 1:
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        break;
-                    case 4:
-                        break;
-                    case 5:
-                        break;
-                }
-            }            
-
+                Console.WriteLine("Nome: {0} - E-mail: {1}", nome[i], email[i]);
+            }
+            Console.WriteLine("\nPressione qualquer tecla para voltar");
+            Console.ReadKey();
         }
     }    
 }
